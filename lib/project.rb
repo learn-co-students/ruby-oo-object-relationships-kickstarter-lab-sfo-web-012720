34703cd3c017abf505f51033cef89be51a97a4a9
austinoso <osorio_austin@yahoo.com>
2020-01-30 13:48:35 -0800

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,24 @@
+require "project_backer.rb"
+
+class Project
+    attr_reader :title
+    def initialize(title)
+        @title = title
+    end
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end
+
+    def search_projects
+        ProjectBacker.all.select do |projects_backed|
+            projects_backed.project ==  self
+        end
+    end
+
+    def backers
+        search_projects.map do |x|
+            x.backer
+        end
+    end
+end
